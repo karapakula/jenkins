5baec89ccddc8bddf4076f4eb56d050e0bbe8060
Narendra Karapakula <Narendra.Karapakula@mphasis.com>
2020-04-01 22:48:51 -0400

    adding new build number 5.6.3.0.43.0-T8 and new region dev2

--- updatemanifest.sh
+++ updatemanifest.sh
@@ -1,8 +1,8 @@
 
 oldbuild="$(cat manifest.xml | grep fileUri | grep ear | cut -f8 -d '/')"
 oldenv="$(cat manifest.xml | grep fileUri | grep ear | cut -f7 -d '/')"
-newenv="dev3"
-newbuild="5.6.3.0.43.0-T9"
+newenv="dev2"
+newbuild="5.6.3.0.43.0-T8"
 
 echo " the old build number is $oldbuild and new build number is $newbuild
 echo " the old env is $oldenv and new env is $newenv
